ccb966044ff99625aa4950942d6a5ccf74dcd6b5
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-07 20:22:42 -0300

    Code refactoring

--- asset/const.js
+++ asset/const.js
@@ -1,4 +1,18 @@
+import { AboutPage } from '../components/AboutPage';
+import { HomePage } from '../components/HomePage';
+
 export const EVENTS = {
   PUSHSTATE: 'pushstate',
   POPSTATE: 'popstate'
 };
+
+export const routes = [
+  {
+    path: '/',
+    Component: HomePage
+  },
+  {
+    path: '/about',
+    Component: AboutPage
+  }
+];
--- components/App.jsx
+++ components/App.jsx
@@ -1,39 +1,12 @@
 import { useEffect, useState } from 'react';
-import { EVENTS } from '../asset/const';
-import { AboutPage } from './AboutPage';
-import { HomePage } from './HomePage';
-
-const routes = [
-  {
-    route: '/',
-    Component: HomePage
-  },
-  {
-    route: '/about',
-    Component: AboutPage
-  }
-];
+import { EVENTS, routes } from '../asset/const';
+import { Page404 } from './404';
+import { Router } from './Router';
 
 export function App () {
-  const [currentPage, setCurrentPage] = useState(window.location.pathname);
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPage(window.location.pathname);
-    };
-
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange);
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange);
-
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange);
-    };
-  }, []);
-
   return (
     <main>
-      {currentPage === '/' && <HomePage />}
-      {currentPage === '/about' && <AboutPage />}
+      <Router route={routes} defaultComponent={Page404} />
     </main>
   );
 }
