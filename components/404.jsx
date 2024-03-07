a841debf7f820299826570cea663243dc80c38ee
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-07 20:20:43 -0300

    Create Link component

---
+++ components/404.jsx
@@ -0,0 +1,8 @@
+export function Page404 () {
+  return (
+    <>
+      <h2>404 but...</h2>
+      <img src='https://midu.dev/images/this-is-fine-404.gif' alt='This is fine gif' />
+    </>
+  );
+}
